f1864d2c015b973654a2b6620903c69450c8f788
FFAI888 <453870150@qq.com>
2025-09-19 08:09:44 +0800

    Create TaskContext.js

---
+++ src/context/TaskContext.js
@@ -0,0 +1,20 @@
+// src/context/TaskContext.js  v0.14
+import React, { createContext, useState } from "react";
+
+export const TaskContext = createContext();
+
+export const TaskProvider = ({ children }) => {
+  const [groupPending, setGroupPending] = useState(false);
+  const [earnPending, setEarnPending] = useState(false);
+  const [exchangePending, setExchangePending] = useState(false);
+
+  return (
+    <TaskContext.Provider value={{
+      groupPending, setGroupPending,
+      earnPending, setEarnPending,
+      exchangePending, setExchangePending
+    }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
